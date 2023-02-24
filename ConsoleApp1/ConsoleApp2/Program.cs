6d324bd3547124ce64568d6b893a4b977012f61e
zixsa <zixsa@DESKTOP-CH3GMM4>
2023-02-24 08:52:46 +0800

    update

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp2
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int? x = null;
+            Console.WriteLine(x);
+            int y;
+            y = x ?? -1;
+            Console.WriteLine(y);
+            float cTemp, fTemp;
+        }
+    }
+}
