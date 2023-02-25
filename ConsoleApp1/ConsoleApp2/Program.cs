24d14515651f92e235d9a6aba9d3742137e31583
zixsa <zixsa@DESKTOP-CH3GMM4>
2023-02-25 21:55:21 +0800

    update

--- ConsoleApp1/ConsoleApp2/Program.cs
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -1,12 +1,21 @@
 ﻿
+using ConsoleApp2.grind_169;
+
 namespace ConsoleApp2
-{ 
+{
     internal class Program
     {
         static void Main(string[] args)
         {
+            /**
             Valid_Para_20 vp20 = new Valid_Para_20();
             Console.WriteLine(vp20.get_result());
+            **/
+
+            /**
+            Merge_two_sorted_list_21 mtsl21 = new Merge_two_sorted_list_21();
+            mtsl21.GetResult();
+            **/
             
         }
     }
