d4b7bf7081ad75927f3d05d8be6ed1665f138eda
zixsa <zixsa@DESKTOP-CH3GMM4>
2023-03-02 22:41:06 +0800

    update

--- ConsoleApp1/ConsoleApp2/grind_169/Invert_bin_226.cs
+++ ConsoleApp1/ConsoleApp2/grind_169/Invert_bin_226.cs
@@ -10,7 +10,16 @@ namespace ConsoleApp2.grind_169
     {
         
         public void GetResult() { 
-            
+            Tree.BinarySearchTree bst = new Tree.BinarySearchTree();
+            int[] list_val = new int[] { 50, 100,25,70,150,60,80,75,90,140,170 };
+            foreach (int i in list_val)
+            {
+                bst.insert(i);
+            }
+            Console.WriteLine("bst insert ending");
+
+            Tree.BinarySearchTree.Visualize(bst.Root,'-');
+
         }
     }
 }
