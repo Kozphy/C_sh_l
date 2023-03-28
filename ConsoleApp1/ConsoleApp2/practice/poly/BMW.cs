9c1e72c61bbc09ee33672d985ab231a205b2576d
zixsa <dbdf0147@gmail.com>
2023-03-28 19:50:53 +0800

    update

---
+++ ConsoleApp1/ConsoleApp2/practice/poly/BMW.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.practice.poly
+{
+    internal class BMW:Car
+    {
+        private string brand = "BMW";
+        public string Model { get; set; }
+        public BMW(int hp, string color, string model) :base(hp, color)
+        {
+           this.Model = model;
+        }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("Brand: " + brand + "HP: " + HP + "Color: " + Color);
+        }
+        public void Repair()
+        {
+            Console.WriteLine($"Model {Model} was repaired");
+        }
+    }
+}
