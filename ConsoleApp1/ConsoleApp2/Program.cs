77663e42c043bfc7a3879f548d5822c9d830fcab
zixsa <dbdf0147@gmail.com>
2023-04-10 20:24:29 +0800

    up

--- ConsoleApp1/ConsoleApp2/Program.cs
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -6,21 +6,11 @@ using ConsoleApp2.practice.abstract_p;
 using ConsoleApp2.practice.Interface_p;
 using ConsoleApp2.practice.Inheritence_p;
 using ConsoleApp2.practice.Enumerate_p;
-<<<<<<< HEAD
-using ConsoleApp2.practice.linq_p;
-using ConsoleApp2.codewar;
-using ConsoleApp2.algo;
-using ConsoleApp2.practice;
-=======
 using ConsoleApp2.practice.poly.para;
 using ConsoleApp2.practice.poly;
-<<<<<<< HEAD
 using ConsoleApp2.practice.linq_p;
 using ConsoleApp2.practice.datetime_p;
 using ConsoleApp2.practice.nullable_p;
-=======
->>>>>>> 00a11085b90aaa26c250ed93d33b52b54bbcdb1f
->>>>>>> 38a469e64971d008e4f30bdc742ff56ab0689c43
 
 namespace ConsoleApp2
 {
@@ -29,20 +19,8 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-<<<<<<< HEAD
             datetime_start.Start();
             var s = new nullable_start();
-=======
-            //select_many.SetectManyEx();
-            //BouncingBall.Start();
-            //fib_set.Start();
-            //var res = fib_dpa.fib(3);
-            //Console.WriteLine(res);
-            Unique_in_order.Start();
-            Yield.Start();
-
-
->>>>>>> 38a469e64971d008e4f30bdc742ff56ab0689c43
         }
     }
 }
