1f3ee68ce22a2e33d14647cee84bf6ff6554e989
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-05-11 20:04:46 +0800

    Delete ConsoleApp1 directory

--- ConsoleApp1/BullyBookWeb/BullyBookWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BullyBookWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
