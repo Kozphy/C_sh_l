56ef7ace5d46a39544a536ad267002c1485bece9
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-06-26 23:15:00 +0800

    up

--- ECPAY/ECPAY/Controllers/HomeController.cs
+++ ECPAY/ECPAY/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
 ﻿using ECPAY.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using ECPay.Payment.Integration;
+using System.Web;
+using System.Security.Cryptography;
+using System.Text;
+using System.ComponentModel;
 
 namespace ECPAY.Controllers
 {
@@ -15,6 +20,56 @@ namespace ECPAY.Controllers
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        
+        private string BuildCheckMacValue(string HashKey, string HashIV,string parameters, int encryptType = 0)
+        {
+            string szCheckMacValue = String.Empty;
+            string reshash = String.Empty;
+            // 產生檢查碼。
+            szCheckMacValue = String.Format("HashKey={0}{1}&HashIV={2}", HashKey, parameters, HashIV);
+            szCheckMacValue = HttpUtility.UrlEncode(szCheckMacValue).ToLower();
+            if (encryptType == 1)
+            {
+                using (SHA256 sha256 = SHA256.Create())
+                {
+                    byte[] hashValue = sha256.ComputeHash(Encoding.UTF8.GetBytes(szCheckMacValue));
+                    foreach (byte b in hashValue) {
+                        reshash += $"{b:X2}";
+                    }
+                }
+            }
+            else
+            {
+                throw new Exception("please input one at EncryptType");
+            }
+            return szCheckMacValue;
+        }
+
+        [HttpPost]
+        public IActionResult CreateOrder([FromBody] ECPayOrderCreateData data) {
+            foreach (PropertyDescriptor desc in TypeDescriptor.GetProperties(data)) {
+                string name = desc.Name;
+                object value = desc.GetValue(data);
+                Console.WriteLine("{0}={1}", name, value);
+            }
+
+
+            Console.WriteLine(data.MerchantID);
+            //Console.WriteLine(data.MerchantID);
+            //Console.WriteLine(data.MerchantTradeNo);
+            //Console.WriteLine(data);
+            //using (AllInOne oPayment = new AllInOne()) {
+            //    oPayment.ServiceMethod = ECPay.Payment.Integration.HttpMethod.HttpPOST;
+            //    oPayment.ServiceURL
+            //}
+            //return View();
+            return Json(data);
+        }
+        
+        public IActionResult OrderReturn() {
             return View();
         }
 
