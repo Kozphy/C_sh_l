f1ef338fd1dbe6b0ee66a411379cb5245f1b60f4
Zixsa <50504962+Kozphy@users.noreply.github.com>
2023-07-18 22:30:11 +0800

    up

--- ECPAY/ECPAY/Models/ECPayOrderCreateDataJSON.cs
+++ ECPAY/ECPAY/Models/ECPayOrderCreateDataJSON.cs
@@ -2,18 +2,22 @@
 
 namespace ECPAY.Models
 {
-    public class ECPayOrderCreateData
+    public partial class ECPayOrderCreateDataJSON
     {
-        [Required]
+
         public string? MerchantID { get; set; }
         public string? MerchantTradeNo { get; set; }
-        public DateTime MerchantTradeDate { get; set; }
+
+        //TODO: need to convert dd/MM/yyyy HH:mm:ss -> yyyy/MM/dd HH:mm:ss
+        public string? MerchantTradeDate { get; set; }
+
         public string? PaymentType { get; set; }
         public int TotalAmount { get; set; }
         public string? TradeDesc { get; set; }
         public string? ItemName { get; set; }
         public string? ReturnURL { get; set; }
         public string? ChoosePayment { get; set; }
-        public string? EncryptType { get; set; }
+        public int EncryptType { get; set; }
+        public string? CheckMacValue { get; set; }
     }
 }
--- ECPAY/ECPAY/wwwroot/js/GenerateOrder.js
+++ ECPAY/ECPAY/wwwroot/js/GenerateOrder.js
@@ -11,9 +11,9 @@ function padTo2DigitsWithZero(num, digit) {
 function formatDate(date) {
     return (
         [
-            date.getFullYear(),
+            padTo2DigitsWithZero(date.getDate()),
             padTo2DigitsWithZero(date.getMonth()),
-            padTo2DigitsWithZero(date.getDate())
+            date.getFullYear()
         ].join('/') +
         ' ' +
         [
@@ -28,30 +28,37 @@ createOrderBtn.addEventListener("click", async function (e) {
     const data = {
         "MerchantID": "3002599",
         "MerchantTradeNo": "123455",
-        "MerchantTradeDate": formatDate(new Date()),
         "PaymentType": "aio",
-        "TotalAmount": 1000,
+        "TotalAmount": "1000",
         "TradeDesc": "test data",
         "ItemName": "棒棒冰",
         "ReturnURL": "http://localhost:5177/Home/OrderReturn",
-        "ChoosePayment":"ALL",
-        "EncryptType":1
+        "ChoosePayment": "ALL",
+        "EncryptType": 1,
     };
-    console.log(data);
 
-    let unEncodeText = `ChoosePayment=${data.ChoosePayment}&EncryptType=${data.EncryptType}&ItemName=${data.ItemName}&MerchantID=${data.MerchantID}&
-    MerchantTradeDate=${data.MerchantTradeDate}&MerchantTradeNo=${data.MerchantTradeNo}&PaymentType=${data.PaymentType}&ReturnURL=${data.ReturnURL}&TradeDesc=${data.TradeDesc}`;
+    //let unEncodeText = `ChoosePayment=${data.ChoosePayment}&EncryptType=${data.EncryptType}&ItemName=${data.ItemName}&MerchantID=${data.MerchantID}&
+    //MerchantTradeDate=${data.MerchantTradeDate}&MerchantTradeNo=${data.MerchantTradeNo}&PaymentType=${data.PaymentType}&ReturnURL=${data.ReturnURL}&TradeDesc=${data.TradeDesc}`;
 
-    data["CheckMacValue"] = unEncodeText;
+    //data["CheckMacValue"] = unEncodeText;
+    console.log("beforeSend: ", data);
 
     try {
+        //let res = await $.ajax({
+        //    type:'POST',
+        //    url: "http://localhost:5177/Home/CreateOrder",
+        //    dataType: "json",
+        //    contentType: "application/json;charset=utf-8",
+        //    data: JSON.stringify(data)
+        //})
 
-        let res = await axios.post("http://localhost:5177/Home/CreateOrder", {
-            headers: {
+        let res = await axios.post("http://localhost:5177/Home/CreateOrder",data,
+            {
+             headers: {
                 "content-type": "application/json;charset=utf-8"
             },
-            data: JSON.stringify(data)
-        })
+        });
+
         console.log(res);
         console.log(res.data);
     } catch (e) {
